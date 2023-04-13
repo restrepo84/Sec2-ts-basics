e7f58020b12748e77edce844e64212a8365574e8
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-13 14:27:51 -0600

    working with enums

--- app.js
+++ app.js
@@ -2,15 +2,40 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Steve",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking", "Basketball"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Steve",
     age: 30,
     hobbies: ["Sports", "Cooking", "Basketball"],
+    role: [2, "author"],
+    role: Role.ADMIN,
 };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- app.ts
+++ app.ts
@@ -2,16 +2,30 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Steve",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking", "Basketball"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Steve",
   age: 30,
-  hobbies: ["Sports", "Cooking", "Basketball"],
+  hobbies: ["Sports", "Cooking"],
   role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
@@ -25,5 +39,9 @@ favoriteActivities = ["Sports"];
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby);
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
